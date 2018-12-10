a55bd7733d7a17378a1f4a00c41951e76abcd9f2
Vika-08 <vikakalinina13@mail.ru>
2018-12-10 22:18:28 +0300

    первая

---
+++ header.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "iostream"
+#include "Windows.h"
+#include "locale"
+#include "string"
+
+using namespace std;
+
+// Sleep(970) - команда, позволяющая "усыпить" консоль на некоторое время (в данном случае на 970мс)
+// system("pause) - команда, останавливающая работу программы до нажатия любой клавиши
+// system("cls") - команда для очистки окна консоли.
+// I'm the best programmer!
